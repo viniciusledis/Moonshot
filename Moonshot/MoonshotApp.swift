102d2938de27a9a5759aecdf86dae0c8985370b5
Vinicius Ledis <161168846+viniciusledis@users.noreply.github.com>
2024-03-12 09:02:37 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Vinicius Ledis on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
